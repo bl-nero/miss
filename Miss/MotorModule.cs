f8f916fca8b1fc50282376b9aaac9c537caa4410
Bartosz Leper <bl.nero@gmail.com>
2014-11-05 00:05:02 +0100

    Add XML comment to MotorModule

--- Miss/MotorModule.cs
+++ Miss/MotorModule.cs
@@ -3,7 +3,13 @@ using Nancy;
 
 namespace Miss {
 
- public class MotorModule: NancyModule {
+  /// <summary>
+  /// A module that is responsible for interacting with motors. It handles URLs
+  /// of a following structure: <c>/v1/motor/[portSpec]/[action]</c>, where
+  /// <c>[portSpec]</c> decides which motor (or pair of motors) will be used,
+  /// and </c>[action]</c> is the name of action to be performed.
+  /// </summary>
+  public class MotorModule: NancyModule {
 
     public MotorModule() : base("/v1/motor") {
       Get["/{portSpec}/switchOn"] = parameters => {
