4964d8782adc0cc62a57144349a47748ce3fce63
Bartosz Leper <bl.nero@gmail.com>
2014-11-16 08:07:41 +0100

    Wire up the server to minimal working state.
    This introduces command-line switches for port number and brick connection and actually connects to the brick.

--- Miss/Program.cs
+++ Miss/Program.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using System.Collections.Generic;
+using MonoBrick.EV3;
 using Nancy;
 using Nancy.Bootstrapper;
 using Nancy.Hosting.Self;
 using Nancy.TinyIoc;
+using Mono.Options;
 
 namespace Miss
 {
     class MissBootstrapper: DefaultNancyBootstrapper
     {
+        private Brick<Sensor, Sensor, Sensor, Sensor> brick;
+
+        public MissBootstrapper(Brick<Sensor, Sensor, Sensor, Sensor> brick)
+        {
+            this.brick = brick;
+        }
+
         protected override void ConfigureApplicationContainer(TinyIoCContainer container)
         {
             base.ConfigureApplicationContainer(container);
@@ -18,10 +27,10 @@ namespace Miss
                 typeof(IDictionary<char, MotorWrapper>),
                 new Dictionary<char, MotorWrapper>()
                 {
-                    { 'a', new MotorWrapper(null) },
-                    { 'b', new MotorWrapper(null) },
-                    { 'c', new MotorWrapper(null) },
-                    { 'd', new MotorWrapper(null) },
+                    { 'a', new MotorWrapper(brick.MotorA) },
+                    { 'b', new MotorWrapper(brick.MotorB) },
+                    { 'c', new MotorWrapper(brick.MotorC) },
+                    { 'd', new MotorWrapper(brick.MotorD) },
                 });
         }
 
@@ -53,11 +62,37 @@ namespace Miss
     {
         public static void Main(string[] args)
         {
-            using (var nancyHost =
-                       new NancyHost(new MissBootstrapper(), new Uri("http://localhost:11550")))
+            int port = 11550;
+            // TODO(bl-nero): Investigate different connection options, especially on Windows, and
+            // refactor this crude setting into something easier to use.
+            string brickConnection = "/dev/tty.EV3-SerialPort";
+
+            var optionSet = new OptionSet()
             {
-                nancyHost.Start();
-                System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+                { "p|port:", (int p) => port = p },
+                { "c|connection:", c => brickConnection = c }
+            };
+            optionSet.Parse(args);
+
+            var brick = new Brick<Sensor, Sensor, Sensor, Sensor>(brickConnection);
+            Console.WriteLine(String.Format("Connecting to the brick at {0}", brickConnection));
+            brick.Connection.Open();
+
+            try
+            {
+                using (var nancyHost = new NancyHost(
+                                       new MissBootstrapper(brick),
+                                       new Uri("http://localhost:" + port)))
+                {
+                    nancyHost.Start();
+                    Console.WriteLine(String.Format("Server started on port {0}.", port));
+                    Thread.Sleep(Timeout.Infinite);
+                }
+            }
+            finally
+            {
+                // I know, I know. Not necessary. Blame my OCD.
+                brick.Connection.Close();
             }
         }
     }
